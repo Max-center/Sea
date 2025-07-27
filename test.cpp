2f757856284f742a64b8684abc735ed2a034a523
Sufikus <166283709+Max-center@users.noreply.github.com>
2025-07-27 13:36:37 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,17 @@
+// SeaExplorer.cpp
+// Фінальний GUI-проєкт WinAPI з синім фоном, списком файлів та вбудованим cmd.exe
+
 #include <windows.h>
 #include <string>
 #include <vector>
 #include <filesystem>
 namespace fs = std::filesystem;
 
-const wchar_t CLASS_NAME[] = L"MyWindowClass";
+const wchar_t CLASS_NAME[] = L"SeaExplorerWindow";
 HWND hListBox;
+HWND hCmdWindow = nullptr;
 
+// Отримує список файлів з C:\Users
 std::vector<std::wstring> GetUserDirectoryFiles() {
     std::vector<std::wstring> files;
     std::wstring path = L"C:\\Users";
@@ -14,10 +19,10 @@ std::vector<std::wstring> GetUserDirectoryFiles() {
     for (const auto& entry : fs::directory_iterator(path)) {
         files.push_back(entry.path().filename().wstring());
     }
-
     return files;
 }
 
+// Заповнює ListBox
 void PopulateListBox(HWND hListBox) {
     std::vector<std::wstring> files = GetUserDirectoryFiles();
     for (const auto& file : files) {
@@ -25,28 +30,43 @@ void PopulateListBox(HWND hListBox) {
     }
 }
 
+// Вбудовує консоль cmd.exe у вікно
+void EmbedCMD(HWND hwndParent) {
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
+
+    wchar_t cmdLine[] = L"cmd.exe /k cd C:\\Users";
+
+    CreateProcessW(nullptr, cmdLine, nullptr, nullptr, FALSE,
+                   CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi);
+
+    Sleep(500); // дати вікну час з'явитись
+
+    HWND hwndCmd = FindWindowW(L"ConsoleWindowClass", nullptr);
+    if (hwndCmd) {
+        SetParent(hwndCmd, hwndParent);
+        SetWindowLong(hwndCmd, GWL_STYLE, WS_VISIBLE | WS_CHILD);
+        MoveWindow(hwndCmd, 20, 320, 300, 200, TRUE);
+        hCmdWindow = hwndCmd;
+    }
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-    case WM_CREATE: {
-        CreateWindowW(L"STATIC", L"You can see all like in a sea ;)", WS_VISIBLE | WS_CHILD,
+    case WM_CREATE:
+        CreateWindowW(L"STATIC", L"You can see all like in a sea ;)",
+                      WS_VISIBLE | WS_CHILD,
                       20, 10, 300, 20, hwnd, nullptr, nullptr, nullptr);
 
-        hListBox = CreateWindowW(L"LISTBOX", nullptr, WS_VISIBLE | WS_CHILD | WS_BORDER | LBS_NOTIFY,
+        hListBox = CreateWindowW(L"LISTBOX", nullptr,
+                                 WS_VISIBLE | WS_CHILD | WS_BORDER | LBS_NOTIFY,
                                  20, 40, 300, 250, hwnd, nullptr, nullptr, nullptr);
 
-        // одразу заповнити список
         PopulateListBox(hListBox);
+        EmbedCMD(hwnd);
         break;
-    }
-
-    case WM_CTLCOLORSTATIC:
-    case WM_CTLCOLORLISTBOX: {
-        HDC hdcStatic = (HDC)wParam;
-        SetBkColor(hdcStatic, RGB(0, 0, 255)); // синій фон
-        SetTextColor(hdcStatic, RGB(255, 255, 255)); // білий текст
-        static HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 255));
-        return (INT_PTR)hBrush;
-    }
 
     case WM_ERASEBKGND: {
         HDC hdc = (HDC)wParam;
@@ -59,10 +79,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     }
 
     case WM_DESTROY:
+        if (hCmdWindow) PostMessage(hCmdWindow, WM_CLOSE, 0, 0);
         PostQuitMessage(0);
         return 0;
     }
-
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
@@ -71,12 +91,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // змінюється вручну в WM_ERASEBKGND
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Sea Explorer", WS_OVERLAPPEDWINDOW,
-                               CW_USEDEFAULT, CW_USEDEFAULT, 360, 360,
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Sea Explorer",
+                               WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 360, 560,
                                nullptr, nullptr, hInstance, nullptr);
 
     if (!hwnd) return 0;
@@ -88,6 +109,5 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return 0;
 }
